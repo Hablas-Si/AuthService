16d96600af4a693205f38cc96dd609d4d7ebd9e6
FynboNohr69420 <kaspernohr@live.dk>
2024-05-26 21:20:41 +0200

    fixes

--- Auctions_AuthService/Program.cs
+++ Auctions_AuthService/Program.cs
@@ -70,20 +70,20 @@ builder.Services.AddAuthorization(options =>
 //tilføjer Repository til services.
 builder.Services.AddSingleton<IVaultService>(vaultService);
 
-// Konfigurer HttpClient for UserService udfra environment variablen UserServiceUrl
-var userServiceUrl = Environment.GetEnvironmentVariable("UserServiceUrl");
-if (string.IsNullOrEmpty(userServiceUrl))
+// Konfigurer HttpClient for AuctionService udfra environment variablen UserServiceUrl
+var auctionServiceUrl = Environment.GetEnvironmentVariable("auctionServiceUrl");
+if (string.IsNullOrEmpty(auctionServiceUrl))
 {
-    logger.Error("UserServiceUrl is missing");
-    throw new ApplicationException("UserServiceUrl is missing");
+    logger.Error("AuctionServiceUrl is missing");
+    throw new ApplicationException("AuctionServiceUrl is missing");
 }
 else
 {
-    logger.Info("UserServiceUrl is: " + userServiceUrl);
+    logger.Info("UserServiceUrl is: " + auctionServiceUrl);
 }
 builder.Services.AddHttpClient<IUserRepository, UserRespository>(client =>
 {
-    client.BaseAddress = new Uri(userServiceUrl);
+    client.BaseAddress = new Uri(auctionServiceUrl);
 });
 
 
